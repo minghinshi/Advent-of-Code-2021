5c0528a4040b232d8118c1ad3e3e8dedae14aeec
minghinshi <mhshi9768@gmail.com>
2021-12-01 18:22:49 +0800

    Day 1 Commit 2
    Increased code efficiency

--- src/com/company/Day01.java
+++ src/com/company/Day01.java
@@ -1,35 +1,29 @@
+/*
+
+*/
+
 package com.company;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 
 public class Day01 {
-
     public static void main(String[] args) throws IOException {
         List<String> content = Files.readAllLines(Paths.get("puzzleInputs/Day01.txt"));
         List<Integer> depths = new ArrayList<>();
-        List<Integer> depthsSmooth = new ArrayList<>();
 
         for (String string : content) {
             depths.add(Integer.parseInt(string));
         }
 
-        for (int i = 0; i < depths.size() - 2; i++) {
-            int sum = 0;
-            for (int j = 0; j < 3; j++) {
-                sum += depths.get(i+j);
-            }
-            depthsSmooth.add(sum);
-        }
-
-        System.out.println("The depth measurement increases " + countIncreases(depths) + " times, without using averages.");
-        System.out.println("The depth measurement increases " + countIncreases(depthsSmooth) + " times, using averages of 3 depths.");
+        System.out.println("The depth measurement increases " + countIncreases(depths,1) + " times, without using averages.");
+        System.out.println("The depth measurement increases " + countIncreases(depths,3) + " times, using averages of 3 depths.");
     }
 
-    public static int countIncreases(List<Integer> integerList){
+    public static int countIncreases(List<Integer> integerList, int slidingWindow){
         int count = 0;
-        for (int i = 1; i < integerList.size(); i++) {
-            if(integerList.get(i) > integerList.get((i-1)))
+        for (int i = slidingWindow; i < integerList.size(); i++) {
+            if(integerList.get(i) > integerList.get((i-slidingWindow)))
                 ++count;
         }
         return count;
